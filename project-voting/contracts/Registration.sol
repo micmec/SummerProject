485965d274ffd9b7be470443068cb926a48c7e89
emilycrussell <53058981+emilycrussell@users.noreply.github.com>
2019-08-19 13:09:25 +0200

    Updated Registration.sol
    removed nationality at this early stage - too complicated

--- project-voting/contracts/Registration.sol
+++ project-voting/contracts/Registration.sol
@@ -9,13 +9,12 @@ contract Registration {
         string lastName;
         uint age;
         string gender;
-        string nationality;
         uint id;
     }
 
     //pre-approved voters are inputted and added onto the list of valid voters.
     //A unique id is generated and outputted to the user - this should be kept secret and will allow registered voters to apply only once to take part in polls later.
-    function addVoter(string memory _firstName, string memory _lastName, uint _age, string memory _gender, string memory _nationality)
+    function addVoter(string memory _firstName, string memory _lastName, uint _age, string memory _gender)
     public returns (uint) {
 
         Voter memory newVoter;
@@ -23,7 +22,6 @@ contract Registration {
         newVoter.lastName = _lastName;
         newVoter.age = _age;
         newVoter.gender = _gender;
-        newVoter.nationality = _nationality;
         //as a starting contract, the generated voterID is basic.
         newVoter.id = voterId++;
 
@@ -32,14 +30,13 @@ contract Registration {
     }
 
     //Not everyone can vote in every poll and so we must retrieve the list of registered voters and form a new list of elligible voters for each individual poll.
-    function getVoters() public view returns (string[] memory, string[] memory, uint[] memory, string[] memory, string[] memory, uint[] memory) {
+    function getVoters() public view returns (string[] memory, string[] memory, uint[] memory, string[] memory, uint[] memory) {
         uint length = voters.length;
 
         string[] memory firstNames = new string[](length);
         string[] memory lastNames = new string[](length);
         uint[] memory ages = new uint[](length);
         string[] memory genders = new string[](length);
-        string[] memory nationalities = new string[](length);
         uint[] memory ids = new uint[](length);
 
         for (uint i = 0; i < voters.length; i++){
@@ -50,11 +47,10 @@ contract Registration {
             lastNames[i] = currentVoter.lastName;
             ages[i] = currentVoter.age;
             genders[i] = currentVoter.gender;
-            nationalities[i] = currentVoter.nationality;
             ids[i] = currentVoter.id;
         }
 
-        return(firstNames, lastNames, ages, genders, nationalities, ids);
+        return(firstNames, lastNames, ages, genders, ids);
     }
 
 }
