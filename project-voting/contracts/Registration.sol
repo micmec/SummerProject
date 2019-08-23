1b760a4de40424cb6dbc57c4d54488a0db8e9e3d
Emily Russell <emilyrussell@Emilys-MBP-7.fritz.box>
2019-08-23 15:16:27 +0200

    No need to define reader function

--- project-voting/contracts/Registration.sol
+++ project-voting/contracts/Registration.sol
@@ -23,28 +23,4 @@ contract Registration {
         return(true);
     }
 
-//     //Not everyone can vote in every poll and so we must retrieve the list of registered voters and form a new list of elligible voters for each individual poll.
-//     function getVoters() public view returns (string[] memory, string[] memory, uint[] memory, string[] memory, uint[] memory) {
-//         uint length = voters.length;
-
-//         string[] memory firstNames = new string[](length);
-//         string[] memory lastNames = new string[](length);
-//         uint[] memory ages = new uint[](length);
-//         string[] memory genders = new string[](length);
-//         uint[] memory ids = new uint[](length);
-
-//         for (uint i = 0; i < voters.length; i++){
-//             Voter memory currentVoter;
-//             currentVoter = voters[i];
-
-//             firstNames[i] = currentVoter.firstName;
-//             lastNames[i] = currentVoter.lastName;
-//             ages[i] = currentVoter.age;
-//             genders[i] = currentVoter.gender;
-//             ids[i] = currentVoter.id;
-//         }
-
-//         return(firstNames, lastNames, ages, genders, ids);
-//     }
-
 }
