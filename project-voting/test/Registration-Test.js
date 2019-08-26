dc31f9a289914250ead540fcb8ba124d09200870
Emily Russell <emilyrussell@Emilys-MBP-7.fritz.box>
2019-08-26 15:31:23 +0200

    Typo fixed

--- project-voting/test/Registration-Test.js
+++ project-voting/test/Registration-Test.js
@@ -3,7 +3,7 @@ var Registration = artifacts.require("./Registration.sol");
 contract("Registration", function(accounts){
     var registrationInstance
 
-    if("adds a voter to mapping", function(){
+    it("adds a voter to mapping", function(){
         Registration.deployed.then(function(instance) {
             instance.addVoter("Emily","Russell", 19, "Female", 0xd66afc18aF7a12937E08Ae6f9a9654Ef1e2b3Dda);})
         addedVoter = Registration.deployed().then(function(instance) {
