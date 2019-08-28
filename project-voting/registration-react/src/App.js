afea8f6c3311e0734f8c8352938d644d6fc80c20
Emily Russell <emilyrussell@Emilys-MacBook-Pro-7.local>
2019-08-28 16:43:03 +0200

    create-react-app and added dependencies

---
+++ project-voting/registration-react/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import ReactDOM from 'react-dom';
+import Web3 from 'web3';
+import TruffleContract from 'truffle-contract';
+import Registration from '../../build/contracts/Registration.json';
+
+class App extends React.Component{
+
+
+}
